0e13321a7334944380f0aa80057131554262920c
Md. Akbar Hossain <smakbor33@gmail.com>
2024-09-18 00:51:30 +0600

    inital

---
+++ Business/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public  required string Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
---
+++ DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Business.Entities;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employes { get; set; }
+    }
+}
